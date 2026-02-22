cb6bd2d7dc8009fbd2842518f68289f1a26ec581
Junaidaliniz <157386475+Junaidaliniz@users.noreply.github.com>
2026-02-22 06:18:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const junaidRouter = require("./api/Junaid.js");
+const junaidRouter = require("./api/junaid.js");
 
 const PORT = process.env.PORT || 3000;
 
